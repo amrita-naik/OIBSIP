6f234940396ab9d5a8ff1fb6e321f10f133db1b1
amrita-naik <naikamrita254@gmail.com>
2022-02-07 22:40:17 +0530

    Task 3

--- Task3/main.js
+++ Task3/main.js
@@ -1,6 +1,10 @@
 const addBtn = document.querySelector('.add-btn')
 const addTask = document.querySelector('.add-task')
 const closeBtn = document.querySelector('.close-btn')
+const saveBtn = document.querySelector('.save-btn')
+const taskList = document.querySelector('.task-list')
+const inputTitle = document.querySelector('.input-title')
+const inputDesc = document.querySelector('.input-desc')
 
 addBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -14,4 +18,23 @@ closeBtn.addEventListener("click", (e) => {
     addTask.classList.remove("show-add-task")
     addBtn.classList.remove("hide-add-btn")
     closeBtn.classList.remove("show-close-btn")
+})
+
+saveBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add('task')
+
+    const newTitle = document.createElement('p');
+    newTitle.innerText= inputTitle.value;
+    newTitle.classList.add('title')
+    taskDiv.appendChild(newTitle)
+
+    const newDesc = document.createElement('p');
+    newDesc.innerText=inputDesc.value;
+    newDesc.classList.add('desc')
+    taskDiv.appendChild(newDesc)
+
+    taskList.appendChild(taskDiv);
 })
