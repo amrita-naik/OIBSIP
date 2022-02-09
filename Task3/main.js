b920eefd301f03f9a5db51de36fc36f934214a32
amrita-naik <naikamrita254@gmail.com>
2022-02-09 18:45:18 +0530

    Task 3

--- Task3/main.js
+++ Task3/main.js
@@ -1,63 +1,44 @@
 window.addEventListener('load', () => {
-    const addBtn = document.querySelector('.add-btn')
-    const addTask = document.querySelector('.add-task')
-    const closeBtn = document.querySelector('.close-btn')
-    const saveBtn = document.querySelector('.save-btn')
+    const taskForm = document.querySelector('.form-control')
     const taskList = document.querySelector('.task-list')
     const inputTitle = document.querySelector('.input-title')
     const inputDesc = document.querySelector('.input-desc')
     const completedList = document.querySelector('.completed-list')
 
-    addBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        addTask.classList.add("show-add-task")
-        addBtn.classList.add("hide-add-btn")
-        closeBtn.classList.add("show-close-btn")
-    })
-
-    closeBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        addTask.classList.remove("show-add-task")
-        addBtn.classList.remove("hide-add-btn")
-        closeBtn.classList.remove("show-close-btn")
-    })
-
-    saveBtn.addEventListener("click", (e) => {
+    taskForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
         const taskDiv = document.createElement("div");
         taskDiv.classList.add('task')
 
         const newTitle = document.createElement('p');
-        newTitle.innerText= inputTitle.value;
-        newTitle.classList.add('title')
+		newTitle.classList.add('title');
+		newTitle.innerHTML = inputTitle.value;
+
         taskDiv.appendChild(newTitle)
 
         const newDesc = document.createElement('p');
-        newDesc.innerText=inputDesc.value;
         newDesc.classList.add('desc')
+        newDesc.innerHTML=inputDesc.value;
+
         taskDiv.appendChild(newDesc)
 
         const newIcons = document.createElement('div')
         newIcons.classList.add('icons')
-
-        const newCheck = document.createElement('button');
-        newCheck.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: #a3d16a;"><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>'
-        newCheck.classList.add('icon')
-        newIcons.appendChild(newCheck)
 
         const newDelete = document.createElement('button');
         newDelete.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" style="fill: #eb5b5b;"><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm10.618-3L15 2H9L7.382 4H3v2h18V4z"></path></svg>'
         newDelete.classList.add('icon')
         newIcons.appendChild(newDelete)
 
+        const newCheck = document.createElement('button');
+        newCheck.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: #a3d16a;"><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>'
+        newCheck.classList.add('icon')
+        newIcons.appendChild(newCheck)
+
         taskDiv.appendChild(newIcons)
 
         taskList.appendChild(taskDiv);
-
-        addTask.classList.remove("show-add-task")
-        addBtn.classList.remove("hide-add-btn")
-        closeBtn.classList.remove("show-close-btn")
 
         inputTitle.value=''
         inputDesc.value=''
@@ -67,8 +48,9 @@ window.addEventListener('load', () => {
         function checkedTask() {
             taskDiv.remove()
             completedList.appendChild(taskDiv)
-            taskDiv.classList.add('checked-task')
             newIcons.removeChild(newCheck)
+            taskDiv.classList.add('checked-task')
+            newTitle.classList.add('pointer-class')
         }
 
         newDelete.addEventListener('click', deleteTask)
@@ -80,13 +62,15 @@ window.addEventListener('load', () => {
             })
         }
 
-        completedList.addEventListener('click', uncheckTask)
+        newTitle.addEventListener('click', uncheckTask)
 
         function uncheckTask() {
-            taskDiv.remove()
             taskList.appendChild(taskDiv)
             taskDiv.classList.remove('checked-task')
+            newTitle.classList.remove('pointer-class')
             newIcons.appendChild(newCheck)
         }
+
+
     })
 })
